11fb9eac98a108de8282c056119940b00c4a8de2
MarioNadal <220056@iessanalberto.com>
2023-10-30 10:16:36 +0100

    "Añadido la persona a la que prestar el libro" 30/10/2023

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 # Clase Libro
 class Libro():
     def __init__(self, titulo, autor, asignatura):
-        self.titulo =  titulo
+        self.titulo = titulo
         self.autor = autor
         self.asignastura = asignatura
         self.prestado = False
+        self.prestadoA = None
 
-    def prestar(self):
+    def prestar(self, persona):
         if self.prestado:
-            print(f"Lo siento el libro {self.titulo} está en préstamo")
+            print(f"Lo siento, el libro {self.titulo} está en préstamo a {self.prestadoA.nombre}")
         else:
-            print(f"Se acaba de prestar el libro {self.titulo}")
             self.prestado = True
+            self.prestadoA = persona
+            print(f"Se acaba de prestar el libro {self.titulo} a {self.prestadoA.nombre}")
 
     def devolver(self):
         if self.prestado:
             self.prestado = False
-            print(f"Se ha devulto el libro {self.titulo}")
+            print(f"{self.prestadoA.nombre} ha devulto el libro {self.titulo}")
         else:
             print(f"El libro {self.titulo} no esta prestado")
 
+
+class Persona():
+    def __init__(self, nombre, apellidos, dni):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.dni = dni
+
+
 quijote = Libro("El Quijote", "Miguel de Cervantes", "IESSAM-01")
 cienyears = Libro("Cien años de soledad", "Gabriel García Márquez", "IESSAM-02")
+alicia = Persona("Alicia", "Anderson", "29345778S")
 
-quijote.prestar()
-quijote.prestar()
+quijote.prestar(alicia)
+quijote.prestar(alicia)
 quijote.devolver()
-quijote.prestar()
+quijote.prestar(alicia)
 quijote.devolver()
 quijote.devolver()
-
-
